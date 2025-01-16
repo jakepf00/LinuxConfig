2b150e36ef03f90b6a83545061182fe91cbf2ff1
Jacob Pfeifer <jakepf00@gmail.com>
2025-01-16 10:54:23 -0700

    nvim options

---
+++ .config/nvim/lua/user/options.lua
@@ -0,0 +1,5 @@
+-- :help options
+vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
+vim.opt.number = true -- set numbered lines
+vim.opt.splitbelow = true -- force all horizontal splits to go below current window
+vim.opt.splitright = true -- force all vertical splits to go right of current window
