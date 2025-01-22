0c5da3e7b7db097098a9ac11457371c0dfabb79e
Jacob Pfeifer <jakepf00@gmail.com>
2025-01-22 08:12:44 -0700

    Add completion

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -45,11 +45,21 @@ return packer.startup(function(use)
 	use "wbthomason/packer.nvim" -- Have packer manage itself
 	use "nvim-lua/popup.nvim" -- An implementation of the Popup API from Vim in Neovim
 	use "nvim-lua/plenary.nvim" -- Useful lua functions used in other plugins
-
 	use "rstacruz/vim-closer" -- Auto close parentheses when enter is pressed
 
 	-- Colorschemes
 	use "folke/tokyonight.nvim"
+
+	-- Completion
+	use "hrsh7th/nvim-cmp" -- The completion plugin
+	use "hrsh7th/cmp-buffer" -- Buffer completions
+	use "hrsh7th/cmp-path" -- Path completions
+	use "hrsh7th/cmp-cmdline" -- Cmdline completions
+	use "saadparwaiz1/cmp_luasnip" -- Snippet completions
+
+	-- Snippets
+	use "L3MON4D3/LuaSnip" -- Snippet engine
+	use "rafamadriz/friendly-snippets" -- A bunch of snippets to use
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	if PACKER_BOOTSTRAP then
