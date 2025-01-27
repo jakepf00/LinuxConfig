6edce249381b1cd6facf60ff972dc2add62d1d9b
Jacob Pfeifer <jakepf00@gmail.com>
2025-01-27 07:47:49 -0700

    Update options

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -6,6 +6,12 @@ local options = {
 	splitbelow = true, -- force all horizontal splits to go below current window
 	splitright = true, -- force all vertical splits to go right of current window
 	termguicolors = true, -- set term gui colors (most terminals support this)
+    showtabline = 2, -- Always show tabs
+	expandtab = true, -- Convert tab to spaces
+	shiftwidth = 4, -- Number of spaces inserted for each indentation
+	tabstop = 4, -- Insert 4 spaces for a tab
+    mouse = "a", -- Allow mouse to be used
+    wrap = false, -- Display lines as one long line
 }
 
 for k, v in pairs(options) do
